4c72ed7fd2ee5eb29da257f7e6f9ff65a1525cc7
andres <andres@localhost.localdomain>
2020-08-17 23:01:08 -0600

    Docker

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/andres/Dev/Tiendas/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ config/gunicorn/conf.py
@@ -0,0 +1,5 @@
+name = 'FabricaABC'
+loglevel = 'info'
+errorlog = '-'
+accesslog = '-'
+workers = 2
